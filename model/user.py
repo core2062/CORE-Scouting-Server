6d024842143460c80f40224b1e270f4ed469223d
Sean Lang <slang800@gmail.com>
2012-05-15 00:35:08 -0500

    still adding basic methods... too many changes to note them all

--- model/user.py
+++ model/user.py
@@ -1,5 +1,7 @@
 import pymongo
 import uuid
+import time
+import web
 
 c = pymongo.Connection()
 db = c.csd
@@ -48,30 +50,36 @@ class user:
 			'browser': '',
 			'gender': '',
 		},
-		'status': {  # info about current session
+		'session': {  # info about current session
 			'ip': '',
-			'logintime': '',  # time when last logged in (when token was issued)
+			'startTime': '',  # time when when token was issued
 			'token': '',
 		},
+		'log': {},
 	}
 
-	def check(self, token, username=''):
+	def check(self, username, token):
 		"""
 			checks username & token and if correct, puts the user object in data
 			used to authenticate user is already logged in (has a token)
+			if an error occurs in this part, the client must run its logout function
 		"""
-		#check token
 
-		#check ip
+		#put it in a temporary variable in case it is incorrect - shouldn't load the user until they are correctly logged in
+		tmpUser = db.user.find_one({'_id': username, 'session.token': token, 'session.ip': web.ctx.ip})
 
-		#consider using random id for normal usernames (login with only email) and special username for admin / guest
-		return 'check not finished'
+		if tmpUser == None:  # means nothing was returned from mongo query
+			raise Exception('incorrect info')  # username & token & ip combo are not correct
+			#CONSIDER: add explanation for why check failed (if it was ip or token or username)
+
+		self.data = tmpUser  # inputs are correct, put user object in correct place
 
 	def login(self, password, username='', email=''):
 		"""
 			checks username / email & password and if correct, generates token and puts user object in data
 			used when user is not yet logged in (has no token)
 			can login with either email or username
+			users cannot be logged in on multiple ip addresses and multiple users cannot be on same ip
 		"""
 
 		if username != '':
@@ -81,20 +89,34 @@ class user:
 		else:
 			raise Exception('need to supply either username or email, none were given')
 
+		query['account.password'] = password  # add password part to query
+
 		#put it in a temporary variable in case it is incorrect - shouldn't load the user until they are correctly logged in
 		tmpUser = db.user.find_one(query)
 
 		if tmpUser == None:  # means nothing was returned from mongo query
-			raise Exception('incorrect username')  # maybe better to only say "incorrect info" to increase security
-
-		if tmpUser['account']['password'] != password:
-			raise Exception('incorrect password')  # maybe better to only say "incorrect info" to increase security
+			raise Exception('incorrect info')  # maybe better to only say "incorrect info" to increase security
 
 		self.data = tmpUser  # inputs are correct, put user object in correct place
 
-		self.data.token = uuid.uuid4()
+		#CONSIDER: check if currently logged in and run logout if true?
 
-		print 'login not finished'
+		#zero out ip & token for users w/ same ip
+		db.user.update(
+			{
+				'ip': self.data['session']['ip']
+			},
+			{
+				'$unset': {
+					'session.ip': 1,
+					'session.token': 1,
+				}
+			},
+		)
+
+		self.data['session']['token'] = str(uuid.uuid4())
+		self.data['session']['ip'] = web.ctx.ip
+		self.data['session']['startTime'] = time.time()
 
 	def logout(self):
 		"""logs out current user by removing ip & token from db"""
@@ -107,6 +129,14 @@ class user:
 	def validateData(self):
 		"""validates user.data - intended to be used for signup and user info changes to determine if user data is acceptable"""
 		return 'validateData not finished'
+
+	def save(self):
+		"""
+			update the representation of the user object in mongo - this is called at the end of the script
+			consider switching to a transparent method of writing to the db
+		"""
+		if self.data['_id'] != 'guest':  # shouldn't save guest account to db because guest isn't a real user
+			db.user.save(self.data)
 
 """
 abc = user()
