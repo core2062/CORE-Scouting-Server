37be5834ea7e349b8d28267ca3cdf4e7014e8415
Sean Lang <slang800@gmail.com>
2012-05-22 22:50:34 -0500

    new module to hold all db interaction

--- model/db.py
+++ model/db.py
@@ -1,6 +1,8 @@
 import pymongo
 
+""" this module establishes the connection to mongo and deals with all db interaction """
+
 c = pymongo.Connection()
-db = c.csd
+csd = c.csd  # variable used in the rest of the code to access the db (for now)
 
 #this might hold some invisible db decorators
