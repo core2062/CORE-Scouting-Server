86892cb251a0e6a37710cec84673ffece43e7a69
Evan <eg1290@gmail.com>
2012-10-28 14:28:39 -0500

    Added mongo-based logging class.

--- model/log.py
+++ model/log.py
@@ -1,2 +1,19 @@
-def error(text):
-	print text
+from mongo_descriptors import Db, MongoI
+from time import time
+
+def error(*text):
+	print ' '.join(text)
+	Log('errors').append(text*)
+
+class Log(object):
+	"""Represents a log document"""
+	db = Db('log', root='csd')
+	log = MongoI('entires',typ=list)
+	raw = MongoI()
+	def __init__(s, oi):
+		s.oi = oi
+		if s.db.find_one(oi)==None:
+			s.db.insert({"_id":oi})
+	def append(s,*entries):
+		logentry = (time(),' '.join(entries))
+		s.log+=[logentry]
