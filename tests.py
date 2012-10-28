000db0f7bedb82acca8e1c22841ab7269242921b
Evan <eg1290@gmail.com>
2012-10-28 15:30:54 -0500

    improved handling errors in tests

--- tests.py
+++ tests.py
@@ -1,11 +1,18 @@
 import requests
+import sys
+
+def die(message):
+	print message
+	sys.exit()
 
 base=lambda x:'http://localhost:5000/'+str(x)
 
 loginParams={'username':'test-user','password':'m3jhrk4'}
-login = requests.get(base('user/login'), params=loginParams)
+login = requests.post(base('user/login'), params=loginParams)
 print login.status_code
 print login.json
+if login.status_code != 200:
+	die('Error in login')
 
 token = login.json['token']
 
