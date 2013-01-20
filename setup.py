499498f8b35d736bc8bec0148ce84d4dad775a12
Sean Lang <slang800@gmail.com>
2013-01-19 23:58:22 -0600

    eh, more stuff

--- setup.py
+++ setup.py
@@ -3,3 +3,11 @@ def defaults():
 	user = new_user("admin", 'mApru', fullname='Kai\'ckul')
 	user.give_perm("*")
 	user = new_user('test-user', 'm3jhrk4')
+
+# move this into the CLI too
+@app.route('/admin/reset', methods=['DELETE'])
+@permission_required('reset_db')
+def reset_db():
+	db.clear()
+	db.defaults()
+	return {'notify': 'reset successful'}
