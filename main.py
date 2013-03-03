fef2b29bd9d1b6aa7fbffa6d81879ffb50012120
Sean Lang <slang800@gmail.com>
2013-03-02 23:16:56 -0600

    removed scrape & reset db from main.py... moving to admin CLI

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from override_flask import NewFlask as Flask
 from werkzeug.exceptions import default_exceptions
 from werkzeug.exceptions import HTTPException
 
-from config import CURRENT_EVENT
-from helper import permission_required
+#from helper import permission_required
 #import model.db as db
 
 import api
@@ -32,7 +31,7 @@ api.mix(app, ['commit', 'user', 'beverages'])
 def before_request():
 	# put the request args in a mutable dict so we can pre-process them
 	g.args = dict(request.args.to_dict())
-	
+
 	if request.json:
 		# if json is sent, merge that into the args
 		g.args.update(request.json)
@@ -45,7 +44,7 @@ def before_request():
 # def after_view(rv):
 # 	# check to see that it's json, if not then return
 # 	if not type(rv) in (dict, list):
-# 		return 	
+# 		return
 # 	#put stuff from g in response
 # 	return
 
@@ -55,25 +54,6 @@ def index():
 	# Eventually, return auto-gen sitemap.
 	return {"message": "huh."}
 
-
-@app.route('/admin/reset', methods=['DELETE'])
-@permission_required('reset_db')
-def reset_db():
-   db.clear()
-   db.defaults()
-   return {'message': 'reset successful'}
-
-
-@app.route('/admin/scrape', methods=['POST'])
-@permission_required()
-def scrape():
-	from scraper import scraper
-	scraper.event_names()
-	scraper.event_details()
-	scraper.tpids()
-	scraper.team_details()
-	scraper.match(CURRENT_EVENT)
-	return {'message': 'scrape successful'}
 
 if __name__ == "__main__":
 	app.run(debug=True)
