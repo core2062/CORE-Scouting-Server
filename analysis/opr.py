791f2acf4adef6e75762687ed6ffa7a25b638697
Sean Lang <slang800@gmail.com>
2013-03-13 08:18:14 -0700

    untested and 1/2 finished implementation of opr calc using least squares

---
+++ analysis/opr.py
@@ -0,0 +1,18 @@
+def opr(entries):
+	# build id map of unique teams
+	team_id_map = []
+	for entry in entries:
+		for team in entry[1:]:
+			if team not in team_id_map:
+				team_id_map.append(team)
+
+	# populate matrix
+	total_teams = len(team_id_map)
+	M = []  # a (2m x total_teams) matrix where m is # of matches
+	s = []  # a (2m x 1) matrix
+	for entry in entries:
+		s.append(entry[1])  # add alliance score
+		row = [0] * total_teams
+		for team in entry[1:]:
+			row[team_id_map.index(team)] = 1
+		M.append(row)
