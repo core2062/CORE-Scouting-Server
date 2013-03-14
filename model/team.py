fa6f4088f87ab1e3004fdd90c1aeaad29f13e9c0
Sean Lang <slang800@gmail.com>
2013-03-13 20:58:28 -0500

    rename

--- model/team.py
+++ model/team.py
@@ -2,3 +2,8 @@
 ### Place to do team-based doc collection
 ### (Should collect every commit related to a team)
 ########################################################
+
+
+class Team(object):
+	def __init__():
+		pass
