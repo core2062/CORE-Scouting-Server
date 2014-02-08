e74a9eb6819f9e58cdb2fd5cdeebd0b5a8c8f624
Alex Krauska <alex@sinusoid.(none)>
2014-02-08 14:48:14 -0600

    Removed user references, updated data collection

--- tests.py
+++ tests.py
@@ -7,50 +7,6 @@ base = lambda x: 'http://localhost:5000' + str(x)
 
 json_headers = {'content-type': 'application/json'}
 
-##################
-# User api tests
-##################
-
-
-@pytest.fixture
-def login(scope="module"):
-	loginParams = {'username': 'test-user', 'password': 'm3jhrk4'}
-	login = requests.post(base('/user/login'), params=loginParams)
-	token = login.json['token']
-	assert token
-	return token
-
-
-def test_login():
-	loginParams = {'username': 'test-user', 'password': 'm3jhrk4'}
-	login = requests.post(base('/user/login'), params=loginParams)
-	print login.status_code
-	print login.json
-	assert login.status_code == 200
-	return login.json['token']
-
-
-def test_accountDetails(login):
-	acctParams = {'token': login}
-	acct = requests.get(base('/user/account'), params=acctParams)
-	# print acct.status_code
-	print acct.text
-	assert acct.json['name'] == 'test-user'
-	assert acct.json['team'] == 0
-
-
-def test_modify(login):
-	from random import random
-
-	newmail = 'newmail' + str(random())
-	modifyData = json.dumps({'email': newmail, 'token': login})
-
-	modify = requests.post(base('/user/update'), data=modifyData, headers=json_headers)
-
-	print modify.status_code
-	print modify.json
-	assert modify.status_code == 200
-	assert 'email' in modify.json['modified']
 
 ###################
 # Commit api tests
@@ -77,26 +33,42 @@ def test_type():
 	assert r.status_code == 200
 	assert r.json
 
+#edit this before the season starts
+
 match_data = {
-		"scout_name": "Test-User",
-		"match_type": "p",
-		"match_num": 42,
-		"team": 0000,
-		"alliance": "Red",
-		"floor_pickup": True,
-		"max_climb": 2,
-		"climb_success": 1,
-		"penalties_red": 0,
-		"penalties_yellow": 0,
-		"fouls": 2,
-		"tech_fouls": 0,
-		"strategy": ["defence", "climb"],
-		"pyramid": 2,
-		"high": 0,
-		"middle": 0,
-		"low": 0,
-		"miss": 0,
-		"comment": "Stupid team."
+	"match_num": 0,
+	"team_num": 0,
+	"match_type": "p",
+	"alliance": "red",
+	"auto_goal": false,
+	"auto_hot": false,
+	"auto_move": false,
+	"auto_goalie": false
+	"auto_shots": 0,
+	"cycles": 0,
+	"total_cycles": 0,
+	"truss_att": 0,
+	"truss_made": 0,
+	"catch_att": 0,
+	"catch_made": 0,
+	"high_att": 0,
+	"high_made": 0,
+	"low_att": 0,
+	"low_made": 0,
+	"pass": 0,
+	"receive": 0,
+	"block": 0,
+	"percent_active": 0,
+	"defense": "NO",
+	"zones": "ALL"
+	"gblocker": false,
+	"pickup": false,
+	"no_show": false,
+	"yellow": 0,
+	"red": 0,
+	"fouls": 0,
+	"tech_fouls": 0,
+	"comments":""
 }
 
 commit_data = {
